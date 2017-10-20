de2e15ec010ccde22efb6c87fda99c3504f811e0
AbZero_Chuang <AbsoluteZero7@163.com>
2017-10-20 17:03:32 +0800

    1. 路由处理完成

--- public/lib/manger/categories-list.js
+++ public/lib/manger/categories-list.js
@@ -1,15 +1,60 @@
-let table;
+var table;
 
 function bindEvent() {
+  $('#showAddPage').on('click', function(e) {
+    e.preventDefault();
+    pop.window('分类管理--添加路由', '/manger/categories_add?index=');
+  });
+
+  $('table').on('click', 'a.option_edit',function(e) {
+    e.preventDefault();
+    let selector = $(e.currentTarget).closest('tr');
+    let data = table.row(selector).data();   
+    pop.window('分类管理--修改路由', '/manger/categories_add?categoryName='+data.name+'&id='+data._id);
+  });
   
+  $('table').on('click', 'a.option_delet', function(e) {
+    e.preventDefault();
+    let selector = $(e.currentTarget).closest('tr');
+    let data = table.row(selector).data();   
+    layer.confirm('您确定删除该路由吗？', function(layero, index) {
+      console.log(layero, index)
+      delCategory([data._id]);
+    });
+  });
+
+  $('#banch_delet').on('click', function(e) {
+    e.preventDefault();
+    let ids = [];
+    const data = table.rows(".selector").data()
+    data.each((data,i) => {
+      ids.push(data._id)
+    })
+    if(ids.length < 1){
+      return layer.msg('请选择数据');
+    }
+    layer.confirm('您是否要删除选中的所有信息？', function() {
+      delCategory(ids);
+    });
+  })
+}
+
+function delCategory(ids) {
+  oajax('/categories/delCategory', 'post', {ids: ids}, function(data) {
+    layer.msg('删除成功',)
+    table.ajax.reload()
+  });
 }
 
 $(function() {
-  let table = initTable({
+  table = initTable({
     url: '/api/categories/getCategory',
     type: 'get',
     datas: function(d) {
-      return {};
+      return {
+        name: $('#categoryName').val(),
+        parent: $('#categoryPName').val(),
+      };
     },
     columns: ['', 'name', 'parent', 'keywords', 'title', 'path', 'order', '_id'],
     defaults: {
@@ -17,15 +62,20 @@ $(function() {
       '2': '----'
     },
     callbacks: {
+      '5': function(data, type, row) {
+        return '<a href="'+ data +'">'+ data +'</a>';
+      },
       '7': function(data, type, row) {
         const ops = [];
-        ops.push('<a class="editor detail" href="javascript:;">详情</a>');
-        ops.push('<a class="editor edit" href="javascript:;">修改</a>');
-        ops.push('<a class="editor delet" href="javascript:;">删除</a>');
+        ops.push('<a class="option_edit" href="javascript:;">修改</a>');
+        ops.push('<a class="option_delet" href="javascript:;">删除</a>');
         return ops.join(' | ');
       }
     }
   });
 
   bindEvent();
+
+
+  
 })
--- routes/api.js
+++ routes/api.js
@@ -7,31 +7,47 @@ const Fragments = require('../models/Fragments');
 // 分类管理
 let path_cate = '/api/categories/';
 
+// 判断输入合法
+function isValid(val) {
+  return (val == '' || val == undefined || val == null ||/^[ ]+$/.test(val)) ? false : true;
+}
+
 // 提取指定参数
 function extract(obj, keys) {
   let tmp = {}; // 对obj遍历 以防obj为空
   _.each(obj, function(val, key) {
-    if(keys.indexOf(keys) === -1) {
+    if(keys.indexOf(key) !== -1 && isValid(val)) {
       tmp[key] = val;
     }
   });
   return tmp;
 }
-// 添加parent 名字
 
+// 图片上传
+router.post('/api/uploadImg', function(req, res) {
+  let files = _.map(req.files, function(f) {
+    return f.path.replace('public', '');
+  });
+  res.json({code: 10, info: files});
+});
+
+// 增加路由 删除路由 修改路由 查找路由
 router.post(path_cate +'addCategory', function(req, res) {
-  let param = req.body;
-  let type = param.type;
-  Categories.createPath(type, param, res, function(msg, info) {
-    res.json({code: msg ? 10 : 11, info: info});
+  let type = req.body.type;
+  if(!type) res.json({code: 11, info: 'category no type!'});
+  let query = extract(req.body, ['name', 'pid', 'keywords', 'title', 'order', 'image', 'comments', 'description']);
+
+  Categories.createPath(type, query, res, function(msg, info) {
+    res.json({code: 10, info: []});
   });
 });
 
 router.post(path_cate +'delCategory', function(req, res) {
   let ids = req.body.ids;
-  Categories.remove({_id: {$all: ids}}, function(err, docs) {
+  Categories.remove({_id: {$in: ids}}, function(err, docs) {
     if(err) {
       res.json({code: 11, info: err.message});
+      return;
     }
     res.json({code: 10, info: []});
   });
@@ -39,32 +55,40 @@ router.post(path_cate +'delCategory', function(req, res) {
 
 router.post(path_cate +'updateCategory', function(req, res) {
   let id = req.body.id;
-  delete req.body.id;
-  let fields = req.body;
-  Categories.findOneAndUpdate({_id: id}, fields, function(err, docs) {
-    if(err) res.json({code: 11, info: err.message});
+  let fields = extract(req.body, ['name', 'pid', 'keywords', 'title', 'order', 'image', 'comments', 'description']);
+  // 增加path
+  if(fields.pid){
+    let cache = res.locals.cache;
+    let path = cache.get(cache.get(fields.pid)).path;
+    fields.path = path +'/'+ fields.name;
+  }
+
+  Categories.findByIdAndUpdate(id, fields, function(err, docs) {
+    if(err) {
+      res.json({code: 11, info: err.message});
+      return;
+    }
     res.json({code: 10, info: []});
   })
 });
 
 router.get(path_cate +'getCategory', function(req, res) {
-  let query = {
-    condi: extract(req.body, ['name', 'parent']),
-    obt: {
-      sort: req.query.sort || -1
-    } 
-  };
+  let tmp = extract(req.query, ['name', 'parent']);
+  // parent转为pid
+  if(tmp.parent) {
+    tmp.pid = res.locals.cache.get(tmp.parent)._id;
+    delete tmp.parent;
+  }
+
+  let query = {condi: tmp, opt: {sort: -1}};
   Categories.getCategory(query, res, function(docs) {
-    let cache = {};
+    // 返回数据加入 parent 字段
     _.each(docs, function(v) {
-      cache[v._id] = v.name;
-    });
-    docs = _.map(docs, function(v) {
-      v._doc.parent = v.pid ? cache[v.pid] : null ;
-      return v;
+      v._doc.parent = res.locals.cache.get(v.pid);
     });
     res.json({code: 10, info: docs});
   });
 });
 
+// 
 module.exports = router;
